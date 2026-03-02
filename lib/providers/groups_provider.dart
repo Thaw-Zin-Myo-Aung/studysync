5a2e4bf93e8e562f0978765aa156c606feda8ddd
Timmythaw <timmythaw17@gmail.com>
2026-03-03 03:43:54 +0700

    Implemented and wired group provider to use group service

--- lib/providers/groups_provider.dart
+++ lib/providers/groups_provider.dart
@@ -1,13 +1,52 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/study_group_model.dart';
+import '../services/firebase/group_service.dart';
+import 'auth_provider.dart';
+
+final groupServiceProvider = Provider<GroupService>((_) => GroupService());
 
 class GroupsNotifier extends Notifier<List<StudyGroupModel>> {
+  late final GroupService _groupService;
+
   @override
-  List<StudyGroupModel> build() => [];
+  List<StudyGroupModel> build() {
+    _groupService = ref.read(groupServiceProvider);
+    // Auto-load when auth state changes
+    ref.listen(authProvider, (_, user) {
+      if (user != null) {
+        loadGroups(user.userId);
+      } else {
+        state = [];
+      }
+    });
+    return [];
+  }
 
-  void setGroups(List<StudyGroupModel> groups) => state = groups;
+  Future<void> loadGroups(String userId) async {
+    try {
+      state = await _groupService.getUserGroups(userId);
+    } catch (_) {
+      state = [];
+    }
+  }
 
-  void addGroup(StudyGroupModel group) => state = [...state, group];
+  Future<void> createGroup({
+    required String name,
+    required String course,
+    required String location,
+  }) async {
+    final group = await _groupService.createGroup(
+      name: name,
+      course: course,
+      location: location,
+    );
+    state = [...state, group];
+  }
+
+  Future<void> joinGroup(String groupId) async {
+    final group = await _groupService.joinGroup(groupId);
+    state = [...state, group];
+  }
 }
 
 final groupsProvider = NotifierProvider<GroupsNotifier, List<StudyGroupModel>>(
